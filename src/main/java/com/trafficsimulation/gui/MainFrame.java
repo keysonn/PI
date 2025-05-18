84f62f7f5b1e8b3f52e000801bf4386782f703b4
PozhidaevNikolay <pozhidaev63nik@gmail.com>
2025-05-19 00:50:31 +0400

    Визуал дороги и тоннеля

--- src/main/java/com/trafficsimulation/gui/MainFrame.java
+++ src/main/java/com/trafficsimulation/gui/MainFrame.java
@@ -48,7 +48,7 @@ public class MainFrame extends JFrame {
 
 
     public MainFrame() {
-        setTitle("Система моделирования движения транспорта v1.5");
+        setTitle("TtafficSimulatiom");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(1000, 700));
